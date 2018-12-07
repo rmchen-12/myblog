dc5b2771776d23c4da035b8d1f2b3a1edd44f1d3
ruiminChen <ruimin.chen@zuolin.com>
2018-12-07 19:21:45 +0800

    加入登录注册

---
+++ app/containers/front/detail/index.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from "react";
+
+class Detail extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>Detail</div>;
+  }
+}
+
+export default Detail;
--- app/containers/front/index.js
+++ app/containers/front/index.js
@@ -5,17 +5,16 @@ import { bindActionCreators } from "redux";
 
 import Test from "./test";
 import Home from "./home";
-import { Detail } from "./detail";
+import Detail from "./detail";
 import Banner from "./components/banner";
 import Menus from "./components/menu";
 import NotFound from "../../components/notFound";
-import Login from "./home/components/login";
-import { logined } from "./home/components/logined";
+import Login from "./components/login";
+import Logined from "./components/logined";
 
 import { actions } from "../../reducers/adminManagerTags";
 import { actions as FrontActions } from "../../reducers/front";
 import { actions as IndexActions } from "../../reducers";
-import Admin from "../admin";
 
 class Front extends PureComponent {
   constructor(props) {
@@ -39,6 +38,10 @@ class Front extends PureComponent {
                 <Route path={"/:tag"} component={Home} />
                 <Route component={NotFound} />
               </Switch>
+            </div>
+            <div>
+              <Login />
+              <Logined />
             </div>
             <div />
           </div>
